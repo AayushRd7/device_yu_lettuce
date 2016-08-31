8e1b97aa2abf40cb155b469fb57275d7bbe511e6
Sagar Shah <sagarshah2326@gmail.com>
2016-08-31 16:06:57 +0000

    lettuce: Enable feature for VR ready devices.

---
+++ product/vr.mk
@@ -0,0 +1,4 @@
+# Virtual Ready
+  PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:system/etc/permissions/android.hardware.vr.high_performance.xml \
+
